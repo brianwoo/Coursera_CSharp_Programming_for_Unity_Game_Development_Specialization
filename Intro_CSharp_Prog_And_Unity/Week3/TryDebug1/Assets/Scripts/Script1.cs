c3666044b7a2bca3121b6d1beb615897feddea84
Brian W <woo.brian+elitebook@gmail.com>
2021-08-31 20:10:38 -0600

    Added launch.json and notes for debugging to work in Linux

---
+++ Intro_CSharp_Prog_And_Unity/Week3/TryDebug1/Assets/Scripts/Script1.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Line 1");
+        Debug.Log("Hi There!");   
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Coursera_CSharp_Programming_for_Unity_Game_Development_Specialization
+
+# To use Mono in VSCode, add:
+"omnisharp.useGlobalMono": "always"
+
+# For debugging (launch.json path removed):
+# Copy launch.json to {project}/.vscode/launch.json
+
+
